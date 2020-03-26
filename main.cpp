f8bddb369b80e51c14f5e0a3246db19c0dbeb051
dbhan08 <38201130+dbhan08@users.noreply.github.com>
2020-03-25 17:55:57 -0700

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -39,59 +39,6 @@ node *getParent(node* root, node* find) {
 
 
 
-// Following function inserts into the tree, also from my binary search tree project
-void insert(node* &root,node* head ,int value) {
-    int RED = 0;
-    int BLACK = 1;
-    // If tree is empty
-    if(root == NULL) {
-        // Make it a new node
-        root = new node(value);
-        root->setColor(BLACK);
-    } else if(value == *head->getValue()) {
-        // If value is already in tree no need for another node
-        return;
-        
-    } else if(value < *head->getValue()) {
-        // If new value is less then the current nodes value
-        
-        if(head->getLeft() == NULL) {
-            // If the left is not null, make a new node with the given value
-            node *temp = new node(value);
-            temp->setColor(RED);
-            head->setLeft(temp);
-            
-        } else {
-            // Transverse to the less
-            insert(root, head->getLeft(),value);
-        }
-    } else {
-        //If given value is greater then current nodes value
-        if(head->getRight() == NULL) {
-            // If the right node is null make new node of the given value
-            node* temp = new node(value);
-            temp->setColor(RED);
-            head->setRight(temp);
-            
-        } else {
-            // Transverse to the right
-            insert(root,head->getRight(),value);
-        }
-        
-        
-    }
-    
-    
-    
-    
-}
-
-
-
-void add(node* &root, int value) {
-    insert(root,root,value);
-    
-}
 
 
 node* getSibling(node *root,node* find) {
@@ -125,6 +72,110 @@ node* getGrand(node* root, node * find) {
         return NULL;
     }
 }
+
+
+// Following function inserts into the tree, also from my binary search tree project
+void insert(node* &root,node* head ,node* temp) {
+    int RED = 0;
+    int BLACK = 1;
+    // If tree is empty
+    if(root == NULL) {
+        // Make it a new node
+        root->setColor(RED);
+        root = temp;
+        
+    } else if(*temp->getValue() == *head->getValue()) {
+        // If value is already in tree no need for another node
+        return;
+        
+    } else if(*temp->getValue() < *head->getValue()) {
+        // If new value is less then the current nodes value
+        
+        if(head->getLeft() == NULL) {
+            // If the left is not null, make a new node with the given value
+            
+            temp->setColor(RED);
+            head->setLeft(temp);
+            
+        } else {
+            // Transverse to the less
+            insert(root, head->getLeft(),temp);
+        }
+    } else {
+        //If given value is greater then current nodes value
+        if(head->getRight() == NULL) {
+            // If the right node is null make new node of the given value
+            
+            temp->setColor(RED);
+            head->setRight(temp);
+            
+        } else {
+            // Transverse to the right
+            insert(root,head->getRight(),temp);
+        }
+        
+        
+    }
+    
+    
+    
+    
+}
+
+void rotateLeft(node* root, node* temp) {
+    node* right = temp->getRight();
+    if(right != NULL) {
+        temp->setRight(right->getRight());
+        if(right->getRight() != NULL) {
+            getParent(rootright->getLeft()) = temp;
+            getParent(root,right) = getParent(root,temp);
+        }
+        if(getParent(root,temp) != NULL) {
+            
+        }
+    }
+}
+
+
+void repairAdd(node* root, node* temp) {
+    int RED = 0;
+    int BLACK = 1;
+    if(getParent(root,temp) == NULL)) {
+        temp ->setColor(BLACK);
+        return;
+    } else if(getParent(root,temp)->getColor() == BLACK) {
+        return;
+    } else if(getParent(root,temp)->getColor() == RED && getUncle(root,temp)->getColor() == RED) {
+        getParent(root,temp)->setColor(BLACK);
+        getUncle(root,temp)->setColor(BLACK);
+        getGrand(root,temp)->setColor(RED);
+        repairAdd(getGrand(root,temp));
+        
+    } else if(getUncle(root,temp)->getColor == BLACK) {
+        if(getGrand(root,temp)->getLeft() == getParent(root,temp)) {
+            if(temp=getParent(root,temp)->getRight()) {
+                
+            }
+            
+        } else if(getGrand(root,temp)->getRight() == getParent(root,temp)) {
+            if(temp=getParent(root,temp)->getLeft()) {
+                
+            }
+            
+        }
+    }
+        
+    
+}
+
+
+
+void add(node* &root, int value) {
+    node* temp = new node(value);
+    insert(root,root,temp);
+    
+}
+
 
 
 
